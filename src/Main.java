c722032435a8a7fa9c41096d3cb9a203badcea61
dariaorlova <orlova77m@gmail.com>
2023-08-29 15:49:06 +0300

    Исправила ДЗ в соответствии с комментариями преподавателя

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("ЗАДАЧА 1");
         System.out.print("Введите ваш возраст: ");
-        Integer age1 = Integer.parseInt(scan.nextLine());
-        if (age1 >= 18) {
+        int age = scan.nextInt();
+        if (age >= 18) {
             System.out.println("Вы являетесь совершеннолетним человеком");
         } else {
             System.out.println("Возраст совершеннолетия еще не наступил");
@@ -14,32 +14,32 @@ public class Main {
 
         System.out.println("ЗАДАЧА 2");
         System.out.print("Введите температуру: ");
-        Integer temperature2 = Integer.parseInt(scan.nextLine());
-        if (temperature2 < 5) {
-            System.out.println("На улице холодно, " + temperature2 + " градусов, нужно надеть шапку");
+        int temperature = scan.nextInt();
+        if (temperature < 5) {
+            System.out.println("На улице холодно, " + temperature + " градусов, нужно надеть шапку");
         } else {
-            System.out.println("Сегодня тепло, " + temperature2 + " градусов, можно идти без шапки");
+            System.out.println("Сегодня тепло, " + temperature + " градусов, можно идти без шапки");
         }
 
         System.out.println("ЗАДАЧА 3");
         System.out.print("Введите скорость: ");
-        Integer speed3 = Integer.parseInt(scan.nextLine());
-        if (speed3 > 60) {
-            System.out.println("Если скорость " + speed3 + " км в час, то придется заплатить штраф");
+        int speed = scan.nextInt();
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + " км в час, то придется заплатить штраф");
         } else {
-            System.out.println("Если скорость " + speed3 + " км в час, то можно ездить спокойно");
+            System.out.println("Если скорость " + speed + " км в час, то можно ездить спокойно");
         }
 
         System.out.println("ЗАДАЧА 4");
         System.out.print("Введите возраст: ");
-        Integer age4 = Integer.parseInt(scan.nextLine());
-        if (age4 >= 2 && age4 <= 6) {
+        age = scan.nextInt();
+        if (age >= 2 && age <= 6) {
             System.out.println("В этом возрасте человек обычно ходит в детский сад");
-        } else if (age4 >= 7 && age4 <= 18) {
+        } else if (age >= 7 && age <= 18) {
             System.out.println("В этом возрасте человек обычно ходит в школу");
-        } else if (age4 > 18 && age4 <= 24) {
+        } else if (age > 18 && age <= 24) {
             System.out.println("В этом возрасте человек обычно ходит в университет");
-        } else if (age4 > 24) {
+        } else if (age > 24) {
             System.out.println("В этом возрасте человек обычно ходит на работу");
         } else {
             System.out.println("Это совсем малыш, в таком возрасте он проводит время с мамой");
@@ -47,36 +47,44 @@ public class Main {
 
         System.out.println("ЗАДАЧА 5");
         System.out.print("Введите возраст: ");
-        Integer age5 = Integer.parseInt(scan.nextLine());
-        if (age5 >= 5 && age5 <= 14) {
+        age = scan.nextInt();
+        if (age >= 5 && age <= 14) {
             System.out.println("Можно кататься на аттракционе только в сопровождении взрослого");
-        } else if (age5 > 14) {
+        } else if (age > 14) {
             System.out.println("Можно кататься на аттракционе без сопровождения взрослого");
         } else {
             System.out.println("Нельзя кататься на аттракционе");
         }
 
         System.out.println("ЗАДАЧА 6");
-        System.out.print("Введите количество занятых мест: ");
-        Integer place6 = Integer.parseInt(scan.nextLine());
-        if (place6 < 60) {
-            System.out.println("В вагоне есть место для сидения");
-        } else if (place6 >= 60 && place6 < 102) {
-            System.out.println("В вагоне есть стоячее место");
+        int places = 102;
+        int seatingPlaces = 60;
+        int standingPlaces = places - seatingPlaces;
+        System.out.print("Введите количество занятых сидячих мест: ");
+        int occupiedSeatingPlaces = scan.nextInt();
+        System.out.print("Введите количество занятых стоячих мест: ");
+        int occupiedStandingPlaces = scan.nextInt();
+        int freeSeatingPlaces = seatingPlaces - occupiedSeatingPlaces;
+        int freeStandingPlaces = standingPlaces - occupiedStandingPlaces;
+        if (occupiedSeatingPlaces < seatingPlaces || occupiedStandingPlaces < standingPlaces) {
+            System.out.println("В вагоне есть места, средни них " + freeSeatingPlaces + " сидячих и " + freeStandingPlaces + " стоячих");
         } else {
             System.out.println("В вагоне нет мест");
         }
 
         System.out.println("ЗАДАЧА 7");
-        int one = 1;
-        int two = 2;
-        int three = 3;
+        System.out.print("Введите первое число: ");
+        int one = scan.nextInt();
+        System.out.print("Введите второе число: ");
+        int two = scan.nextInt();
+        System.out.print("Введите третье число: ");
+        int three = scan.nextInt();
         if (one > two && one > three) {
-            System.out.println("Самое большое число one");
-        } else if (two > one && two > three) {
-            System.out.println("Самое большое число two");
+            System.out.println("Самое большое число " + one);
+        } else if (two > three) {
+            System.out.println("Самое большое число " + two);
         } else {
-            System.out.println("Самое большое число three");
+            System.out.println("Самое большое число " + three);
         }
     }
 }
